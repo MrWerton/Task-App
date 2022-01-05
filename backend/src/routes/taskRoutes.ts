7546bed652e32754faee4376e2ea58712512357f
MrWerton <mr.werton@gmail.com>
2022-01-05 02:33:29 -0300

    Created the routes

---
+++ backend/src/routes/taskRoutes.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { TaskCtrl} from '../controllers/TaskCtrl';
+import {validateid} from '../middlewares/TaskMdlw'
+const TaskRoute = express.Router();
+
+TaskRoute.get('/', TaskCtrl.getTask);
+TaskRoute.post('/', TaskCtrl.postTask);
+TaskRoute.put('/:id',validateid, TaskCtrl.updateTask);
+TaskRoute.delete('/:id',validateid, TaskCtrl.DeleteTask);
+
+
+
+
+export {TaskRoute}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,16 @@ import express, {json} from 'express'
 import cors from 'cors'
 require('dotenv').config()
 import {conectionDatabase} from './database'
+import {TaskRoute} from '../src/routes/taskRoutes'
 
 conectionDatabase()
 const app = express();
 
 app.use(json())
 app.use(cors())
+app.use('/task', TaskRoute)
 
-const port = process.env.PORT || '8070'
+const port = process.env.PORT || '8080'
 
 
 app.listen(port, ()=>{
