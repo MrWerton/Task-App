d445c6e79324ce388e19b621b81ba2469cb4344b
MrWerton <mr.werton@gmail.com>
2022-01-05 02:31:26 -0300

    Created middleware for task

---
+++ backend/src/middlewares/TaskMdlw.ts
@@ -0,0 +1,21 @@
+import {TaskModel} from '../models/TaskModel'
+import {validate as isUiid} from 'uuid'
+import {Request, Response, NextFunction} from 'express'
+ const validateid = async ( req:Request, res:Response, next:NextFunction)=>{
+	const {id} = req.params;
+	if(!isUiid(id)){
+		return res.status(400).json({error: "this id don'ts valid"})
+	}
+	try{
+		const task = await TaskModel.findById(id)
+		if(!task){
+			return res.status(404).json({msg: "user not found"})
+		}else{
+			return next()
+		}	
+	}catch(err:any){
+		return res.status(500).json({err: err.message})
+	}
+}
+
+export {validateid}
