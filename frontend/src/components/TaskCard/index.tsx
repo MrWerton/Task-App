ecb5fcbee09bfa3526b1dd044d68c21c2ba31a80
MrWerton <mr.werton@gmail.com>
2022-01-06 15:18:49 -0300

    created card components

--- frontend/src/components/TaskCard/index.tsx
+++ frontend/src/components/TaskCard/index.tsx
@@ -1,5 +1,10 @@
-import { Container, Title,
-  Description  } from './styles';
+import { Container, Wrapper, Title,
+  Description, Content, Header,Btn, CheckBox 
+} from './styles';
+import {ImCheckboxUnchecked} from 'react-icons/im'
+import {ImCheckboxChecked} from 'react-icons/im'
+import {FiMoreVertical} from 'react-icons/fi'
+
 
 interface Itask{
   title: string,
@@ -12,10 +17,21 @@ const TaskCard = ({description, state, title}:Itask) => {
  
   return (
     <Container>
-      {state? <p>going</p>: <p>doing</p>}
-      <input type="checkbox" />
-      <Title>{title}</Title>
-      <Description>{description}</Description>
+      <Header>
+         <Wrapper>
+           <CheckBox>
+           {state?<ImCheckboxChecked/>: <ImCheckboxUnchecked/>}
+           </CheckBox>
+           <Title>{title}</Title>
+           </Wrapper>
+         <Btn>
+           <FiMoreVertical/>
+         </Btn>
+      </Header>
+      <Content>
+          <Description>{description}</Description>
+      </Content>
+     
     </Container>
   );
 };
--- frontend/src/components/TaskCard/styles.ts
+++ frontend/src/components/TaskCard/styles.ts
@@ -1,16 +1,54 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    width: 400px;
-    height: 90px;
-    background: #212121;
+    width: 100%;
+    height: auto;
+    background: #1F2937;
+    border-radius: 10px;
+    
 `;
-export const Title = styled.h2`
+export const Wrapper = styled.div`
+    font-size: 22px;
+    color: #dadada;
+    gap: 15px;
+    display: flex;
+    align-items: center;
+`
+export const Title = styled.h3`
     font-size: 24px;
-    color: #dada;
+    color: #dadada;
+  
+    
+`
+export const CheckBox = styled.div`
+    font-size: 22px;
+    color: red;
+  
+    
 `
 export const Description = styled.p`
     font-size: 16px;
-    color: #dada;
+    color: #dadada;
+
+`
+export const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 15px;
+    align-items: center;
+
+`
+export const Content = styled.div`
+    font-size: 16px;
+    color: #dadada;
+    padding: 15px;
+    display: flex;
+    align-items: center;
+
+`
+export const Btn = styled.div`
+    font-size: 16px;
+    color: #dadada;
 
 `
