63ebddfabff41cdb8ab7c15a66c4cbb41c10ea16
MrWerton <mr.werton@gmail.com>
2022-01-06 18:24:32 -0300

    Created form for add and update of the task

--- frontend/src/components/TaskForm/index.tsx
+++ frontend/src/components/TaskForm/index.tsx
@@ -1,9 +1,25 @@
-import { Container } from './styles';
+import { Container, IconClose, Button, Form, Box } from './styles';
 
 const TaskForm = () => {
   return (
     <Container>
-      <h1>CreateTaskForm</h1>
+      <Box>
+          <IconClose/>
+          <Form>
+                <label>
+                  <h3>Title</h3>
+                  <input type="text" placeholder='title' />
+                </label>
+                <label>
+                  <p>Description</p>
+                  <textarea name="description" id="description" placeholder='description'></textarea>
+                </label>
+             
+          </Form>
+          <Button>
+            add
+          </Button>
+      </Box>
     </Container>
   );
 };
--- frontend/src/components/TaskForm/styles.ts
+++ frontend/src/components/TaskForm/styles.ts
@@ -1,5 +1,69 @@
 import styled from 'styled-components';
+import {IoIosCloseCircle} from 'react-icons/io'
 
 export const Container = styled.div`
-
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(17,24,39,0.81);
+    position: fixed;
+    z-index: 111111;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
 `;
+export const Box = styled.div`
+position: relative;
+    background-color: #1F2937;
+    width: 400px;
+    height: 450px;
+    border-radius: 10px;
+    padding: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
+export const IconClose = styled(IoIosCloseCircle)`
+    position: absolute;
+    right: 15px;
+    top: 15 px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 40px;
+    color: #dadada;
+    cursor: pointer;
+`
+export const Button = styled.button`
+    margin-top: 20px;
+    padding: 12px 30px;
+    color: #111827;
+    background-color: #dadada;
+    font-weight: bold;
+    border-radius: 10px;
+    cursor: pointer;
+`
+export const Form = styled.form`
+    margin: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 20px;
+    input{
+        margin-top: 10px;
+        background-color: #111827;
+        border-radius: 10px;
+        padding: 10px;
+    }
+    textarea{
+        margin-top: 10px;
+        background-color: #111827;
+        border-radius: 10px;
+        padding: 10px;
+        height: 120px;
+    }
+
+`
